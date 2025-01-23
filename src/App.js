5a40e3dc744c0949a7d2351caedf995d1f33e008
sobogert <bogertsophia@gmail.com>
2025-01-22 22:09:02 -0700

    worked on expanded recipe

--- src/App.js
+++ src/App.js
@@ -16,41 +16,42 @@ function App() {
         'maybe potatos?'
     ];
 
-    const thaiCurrySteps = 'cut up your potatos and your kale/broccoli and then cut up your ginger I mean your broccoli I mean your garlic, but keep that separate\n' +
-        'alright\n' +
-        'and then (hahahah) I forget. do you have a fine grater? cool ok then cut up the ginger and put it in the same pile as the garlic. you want that minced\n' +
-        'alright\n' +
-        'so then uh uh drain your benzos\n' +
-        'alright no we’re ready to start\n' +
-        'sear the chicken both sides in butter, good amount of butter\n' +
-        'and then put it on a plate\n' +
-        'and then dump in broccoli and potatos, stir around\n' +
-        'um\n' +
-        'then put in the bonzos\n' +
-        'stirthemaround\n' +
-        'uhhhhhhhhhh\n' +
-        'open up teh curry paste\n' +
-        'and then you’re putting in the curry paste, teh ginger, adn teh garlic all at the same time\n' +
-        'and stiritaround\n' +
-        'and then place the chicken on top of your vegetable-bonzo mixture\n' +
-        'burp\n' +
-        'pour coconut milk ontop\n' +
-        'stir around\n' +
-        'and then you just like maybe add some depending on how much coverage you have add a little bit of water and bullion\n' +
-        'that’s it\n' +
-        'and then you let it simmerforanhour\n' +
-        'so you wanna put it on your simmering burner instead of your burning burner\n' +
-        'youknowwhwatImean\n' +
-        'that’s it\n' +
-        'I mean that’s not all of it\n' +
-        'but I’ll get you like 95% of the way oh and lime goes in at some point, any point\n' +
-        '96%\n' +
-        'one teaspoon of bullion per cup\n' +
-        'or follow instructions';
+    const thaiCurrySteps = [
+        'cut up your potatos and your kale/broccoli and then cut up your ginger I mean your broccoli I mean your garlic, but keep that separate',
+        'alright',
+        'and then (hahahah) I forget. do you have a fine grater? cool ok then cut up the ginger and put it in the same pile as the garlic. you want that minced',
+        'alright',
+        'so then uh uh drain your benzos',
+        'alright no we’re ready to start',
+        'sear the chicken both sides in butter, good amount of butter',
+        'and then put it on a plate',
+        'and then dump in broccoli and potatos, stir around',
+        'um',
+        'then put in the bonzos',
+        'stirthemaround',
+        'uhhhhhhhhhh',
+        'open up teh curry paste',
+        'and then you’re putting in the curry paste, teh ginger, adn teh garlic all at the same time',
+        'and stiritaround',
+        'and then place the chicken on top of your vegetable-bonzo mixture',
+        'burp',
+        'pour coconut milk ontop',
+        'stir around',
+        'and then you just like maybe add some depending on how much coverage you have add a little bit of water and bullion',
+        'that’s it',
+        'and then you let it simmerforanhour',
+        'so you wanna put it on your simmering burner instead of your burning burner',
+        'youknowwhwatImean',
+        'that’s it',
+        'I mean that’s not all of it',
+        'but I’ll get you like 95% of the way oh and lime goes in at some point, any point',
+        '96%',
+        'one teaspoon of bullion per cup',
+        'or follow instructions'];
 
   return (
     <div className="flex flex-column flex-center flex-align-center page">
-      <header className="header">Joe's Recipe Book</header>
+      <h1 className="header">Joe's Recipe Book</h1>
         <div className={"page-body flex flex-column flex-start"}>
             <Recipe
                 recipeName={'Thai Curry'}
--- src/components/recipe.js
+++ src/components/recipe.js
@@ -6,40 +6,77 @@ export default function Recipe(props) {
     let recipeName = props.recipeName;
     let estCookTime = props.estCookTime;
     let estTotalTime = props.estTotalTime;
-
+    let ingredients = props.ingredients.map(ingredient => <li>{ingredient}</li>);
+    let steps = props.steps.map(step => <li>{step}</li>);
     let recipeBodyClassName = 'recipe_body display-none';
     let recipeContainerClass = 'recipe flex flex-column recipe_condensed';
-    
-    if (!isHidden) {
-        recipeBodyClassName = 'recipe_body';
-        recipeContainerClass = "recipe flex flex-column recipe_expanded";
-    } else {
-        recipeBodyClassName = 'recipe_body display-none';
-        recipeContainerClass = 'recipe flex flex-column recipe_condensed';
-    }
 
     function handleClick() {
         setIsHidden(!isHidden);
+        // if (!isHidden) {
+        //     recipeBodyClassName = 'recipe_body';
+        //     recipeContainerClass = "recipe flex flex-column recipe_expanded";
+        // } else {
+        //     recipeBodyClassName = 'recipe_body display-none';
+        //     recipeContainerClass = 'recipe flex flex-column recipe_condensed';
+        // }
     }
 
-    return (
-        <div className={recipeContainerClass} onClick={handleClick}>
-            <h2 className={"recipe_header"}>{recipeName}</h2>
-            {/* main recipe card can have name, cook time, and maybe # ingredients
+    if (isHidden) {
+        return (
+            <div className={"recipe flex flex-column recipe_condensed"} onClick={handleClick}>
+                <h2 className={"recipe_header"}>{recipeName}</h2>
+                {/* main recipe card can have name, cook time, and maybe # ingredients
                 then expand to show details */}
-            <div className={"recipe_subtitle-container flex flex-column"}>
-                <h3 className={"recipe_subtitle flex"}>est cook time: {estCookTime}</h3>
-                <h3 className={"recipe_subtitle flex"}>est total time: {estTotalTime}</h3>
-            </div>
+                <div className={"recipe_subtitle-container flex flex-column"}>
+                    <h3 className={"recipe_subtitle flex"}>est cook time: {estCookTime}</h3>
+                    <h3 className={"recipe_subtitle flex"}>est total time: {estTotalTime}</h3>
+                </div>
 
-            <div className={recipeBodyClassName}>
-                this is here now
+                <div className={"recipe_body display-none"}>
+                    this is here now
 
-            {/*    have ingredients and instructions here
+                    {/*    have ingredients and instructions here
             on click, change state of the component to show the
             hidden fields, expand the parent container, show the close btn*/}
+                </div>
             </div>
-        </div>
 
-    );
+        );
+    }
+    else {
+        return (
+            <div className={"recipe flex flex-column recipe_expanded"}>
+                <div className={"flex flex-between"}>
+                    <h2 className={"recipe_header"}>{recipeName}</h2>
+                    <button className={"close-btn"} onClick={handleClick}>X</button>
+                </div>
+                {/* main recipe card can have name, cook time, and maybe # ingredients
+                then expand to show details */}
+
+
+                <div className={"recipe_body"}>
+                    <div className={"recipe_body_subsection flex flex-column flex-start"}>
+                        <h3>Ingredients</h3>
+                        <div className={"recipe_ingredients flex flex-start flex-column"}>
+                            {ingredients}
+                        </div>
+                    </div>
+
+                    <div className={"recipe_body_subsection flex flex-column flex-start"}>
+                        <h3>Steps</h3>
+                        <div className={"recipe_steps flex flex-start flex-column"}>
+                            {steps}
+                        </div>
+                    </div>
+
+                    {/*    have ingredients and instructions here
+            on click, change state of the component to show the
+            hidden fields, expand the parent container, show the close btn*/}
+                </div>
+            </div>
+
+        );
+    }
+
 }
